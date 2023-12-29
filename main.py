6e6c8cf66ea537ba0433ccf4a25c23c98d1efa80
Wayan <wayaninwonderland@mail.com>
2023-12-29 10:10:50 +0700

    final changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
-
+import os
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
@@ -26,7 +26,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -282,4 +282,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5100)
+    app.run(debug=False)
